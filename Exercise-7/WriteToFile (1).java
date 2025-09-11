c58047aadb49c5d5bedcc1f87c7f8b031ecbe8b2
Kumaran S <samkumaran005@gmail.com>
2025-09-11 10:42:57 +0530

    Update WriteToFile (1).java

--- Exercise-7/WriteToFile (1).java
+++ Exercise-7/WriteToFile (1).java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package FileInputOutput;
+import java.io.FileWriter;
+import java.io.IOException;
 
-/**
- *
- * @author student
- */
 public class WriteToFile {
-    
+    public static void main(String[] args) {
+        try (FileWriter writer = new FileWriter("mydata.txt")) {
+            writer.write("Hello, Java Streams!");
+            System.out.println("Data written to mydata.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
