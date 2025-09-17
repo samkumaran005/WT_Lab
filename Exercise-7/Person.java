d61846d219829bb7d67b8cec4b1d2448036c51dd
Kumaran S <samkumaran005@gmail.com>
2025-09-17 10:45:21 +0530

    Update Person.java

--- Exercise-7/Person.java
+++ Exercise-7/Person.java
@@ -1,13 +1,40 @@
-import java.io.Serializable;
-public class Person implements Serializable {
-    private final String name;
-    private final int age;
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+import java.io.*;
+public class PersonExample {
+    public static class Person implements Serializable {
+        private final String name;
+        private final int age;
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+        @Override
+        public String toString() {
+            return "Person[name=" + name + ", age=" + age + "]";
+        }
     }
-    @Override
-    public String toString() {
-        return "Person[name=" + name + ", age=" + age + "]";
+    public static void writePersonToFile(String filename, Person person) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+            oos.writeObject(person);
+            System.out.println("Person object saved.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing: " + e.getMessage());
+        }
+    }
+    public static Person readPersonFromFile(String filename) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            return (Person) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("An error occurred while reading: " + e.getMessage());
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        Person person = new Person("Alice", 30);
+        String filename = "person.dat";
+        writePersonToFile(filename, person);
+        Person readPerson = readPersonFromFile(filename);
+        if (readPerson != null) {
+            System.out.println("Read person: " + readPerson);
+        }
     }
 }
