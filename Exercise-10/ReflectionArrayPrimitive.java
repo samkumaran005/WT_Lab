eebaf18854ac915e9572db446c8936a7c28adc7c
Kumaran S <samkumaran005@gmail.com>
2025-09-25 09:20:27 +0530

    Delete Exercise-10 directory

--- Exercise-10/ReflectionArrayPrimitive.java
+++
@@ -1,9 +0,0 @@
-public class ReflectionArrayPrimitive {
-    public static void main(String[] args) {
-        Class<?> intClass = int.class;
-        Class<?> arrayClass = int[].class;
-        System.out.println("int.class isPrimitive: " + intClass.isPrimitive());
-        System.out.println("int[].class isArray: " + arrayClass.isArray());
-        System.out.println("int[].class Name: " + arrayClass.getName());
-    }
-}
