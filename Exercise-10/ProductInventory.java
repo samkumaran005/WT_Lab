8400b7ad0b2fc574271147981a0f7e2249bc87a4
Kumaran S <samkumaran005@gmail.com>
2025-10-20 15:16:23 +0530

    Update ProductInventory.java

--- Exercise-10/ProductInventory.java
+++ Exercise-10/ProductInventory.java
@@ -6,10 +6,8 @@ public class ProductInventory {
         productMap.put("Banana", 15);
         productMap.put("Orange", 8);
         productMap.put("Mango", 20);
-        for (Map.Entry<String, Integer> entry : productMap.entrySet()) {
-            if (entry.getValue() > 10) {
-                System.out.println(entry.getKey() + ": " + entry.getValue());
-            }
-        }
+        productMap.forEach((k,v)->{
+        if(v>10) System.out.println(k+":"+v);
+        });
     }
 }
