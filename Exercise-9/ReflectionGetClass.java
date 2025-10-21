e418036e8ed977ec8323abbf69075c61beddbb76
Kumaran S <samkumaran005@gmail.com>
2025-10-21 11:29:18 +0530

    Update ReflectionGetClass.java

--- Exercise-9/ReflectionGetClass.java
+++ Exercise-9/ReflectionGetClass.java
@@ -1,27 +1,29 @@
+package reflections;
+import java.lang.reflect.*;
 class Sample {
     public String name;
     public Sample() {}
     public Sample(int id) {}
-    public void display() {}
+    private void display() {}
 }
 public class ReflectionGetClass {
     public static void main(String[] args) throws Exception {
         Sample obj = new Sample();
-        Class<?> clazz = obj.getClass();
+        Class clazz = obj.getClass();
         System.out.println("Class Name: " + clazz.getName());
         System.out.println("Superclass: " + clazz.getSuperclass().getName());
         System.out.println("Is Interface: " + clazz.isInterface());
         System.out.println("Is Array: " + clazz.isArray());
         System.out.println("Is Primitive: " + clazz.isPrimitive());
         System.out.println("\nDeclared Fields:");
-        for (java.lang.reflect.Field field : clazz.getDeclaredFields()) {
+        for (Field field : clazz.getDeclaredFields()) {
             System.out.println(field.getName() + " - Modifier: " + field.getModifiers());
         }
         System.out.println("\nDeclared Methods:");
-        for (java.lang.reflect.Method method : clazz.getDeclaredMethods()) {
+        for (Method method : clazz.getDeclaredMethods()) {
             System.out.println(method.getName() + " - Modifier: " + method.getModifiers() + " - Return Type: " + method.getReturnType());
         }
-        java.lang.reflect.Method m = clazz.getDeclaredMethod("secret");
+        Method m = clazz.getDeclaredMethod("display");
         m.setAccessible(true);
         m.invoke(obj);
     }
