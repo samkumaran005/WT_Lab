19483762e677f78c0f080d7ce0058282625caa90
Kumaran S <samkumaran005@gmail.com>
2025-10-21 11:24:40 +0530

    Update ReflectionForName.java

--- Exercise-9/ReflectionForName.java
+++ Exercise-9/ReflectionForName.java
@@ -1,15 +1,28 @@
+package reflections;
+import java.lang.reflect.*;
+class Sample{
+    private int id;
+    private String name;
+    public Sample(){
+        System.out.println("this is the constructor");
+    }
+    public Sample(int n){
+        System.out.println("this is the constructor of value"+n);
+    }
+}
 public class ReflectionForName {
     public static void main(String[] args) throws Exception {
-        Class<?> clazz = Class.forName("Sample");
+        Sample obj = new Sample();
+        Class clazz = Class.forName("reflections.Sample");
         System.out.println("Declared Constructors:");
-        for (java.lang.reflect.Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+        for (Constructor constructor : clazz.getDeclaredConstructors()) {
             System.out.println(constructor.getName() + " - Modifier: " + constructor.getModifiers() + " - Parameter Count: " + constructor.getParameterCount());
         }
-        java.lang.reflect.Constructor<?> c = clazz.getDeclaredConstructor(int.class);
+        Constructor c = clazz.getDeclaredConstructor(int.class);
         System.out.println("Specific Constructor: " + c.getName());
-        java.lang.reflect.Field f = clazz.getDeclaredField("id");
+        
+        Field f = clazz.getDeclaredField("id");
         f.setAccessible(true);
-        Sample obj = new Sample();
         f.set(obj, 101);
         System.out.println("Private Field Value: " + f.get(obj));
     }
