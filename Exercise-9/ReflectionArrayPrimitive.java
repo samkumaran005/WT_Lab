38be5d2bd4c7620ed558f3e20ee905676eb130ed
Kumaran S <samkumaran005@gmail.com>
2025-10-21 11:01:20 +0530

    Update ReflectionArrayPrimitive.java

--- Exercise-9/ReflectionArrayPrimitive.java
+++ Exercise-9/ReflectionArrayPrimitive.java
@@ -1,9 +1,10 @@
 public class ReflectionArrayPrimitive {
     public static void main(String[] args) {
-        Class<?> intClass = int.class;
-        Class<?> arrayClass = int[].class;
+        Class intClass = int.class;
+        Class arrayClass = int[].class;
         System.out.println("int.class isPrimitive: " + intClass.isPrimitive());
         System.out.println("int[].class isArray: " + arrayClass.isArray());
         System.out.println("int[].class Name: " + arrayClass.getName());
     }
+
 }
